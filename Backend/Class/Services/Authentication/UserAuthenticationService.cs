d693c7d69385ff7c326c6960c524f4df63c8797b
Felaeia <elizabeth2ndcando@gmail.com>
2025-10-02 12:44:33 +0800

    Feat: + Converted UserAuthenticationServoce SQL Commands to Stored Procedure inside SSMS + Added new Stored Procedure txt file LoginUserAuth - Changed TruncateAllTable txt file sql command from ALTER to CREATE

--- Backend/Class/Services/Authentication/UserAuthenticationService.cs
+++ Backend/Class/Services/Authentication/UserAuthenticationService.cs
@@ -31,8 +31,9 @@ namespace NurseRecordingSystem.Class.Services.Authentication
             }
 
             using (var connection = new SqlConnection(_connectionString))
-            using (var cmdLoginUser = new SqlCommand("SELECT authId, userName, passwordHash,passwordSalt, email, role FROM [Auth] WHERE email = @email", connection))
+            using (var cmdLoginUser = new SqlCommand("dbo.LoginUserAuth", connection))
             {
+                cmdLoginUser.CommandType = System.Data.CommandType.StoredProcedure;
                 cmdLoginUser.Parameters.AddWithValue("@email", request.Email);
                 try
                 {
