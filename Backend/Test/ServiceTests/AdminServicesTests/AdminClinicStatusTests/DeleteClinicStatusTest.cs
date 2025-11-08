88dffea51c0bb2577a28c084fd0e6a574c817d9a
Serebus <revontheusmercer@gmail.com>
2025-11-08 15:03:13 +0800

    Refactor: + change it to include mock db

--- Backend/Test/ServiceTests/AdminServicesTests/AdminClinicStatusTests/DeleteClinicStatusTest.cs
+++ Backend/Test/ServiceTests/AdminServicesTests/AdminClinicStatusTests/DeleteClinicStatusTest.cs
@@ -1,6 +1,8 @@
 using Moq;
 using NurseRecordingSystem.Class.Services.ClinicStatusServices;
 using Xunit;
+using Microsoft.Data.SqlClient;
+using System.Data;
 
 namespace NurseRecordingSystem.Test.ServiceTests.AdminServicesTests.AdminClinicStatusTests
 {
@@ -17,6 +19,34 @@ namespace NurseRecordingSystem.Test.ServiceTests.AdminServicesTests.AdminClinicS
 
             // Act & Assert
             await Assert.ThrowsAsync<InvalidOperationException>(() => new DeleteClinicStatus(mockConfiguration.Object).DeleteAsync(1, "test"));
+        }
+
+        [Fact]
+        public async Task DeleteAsync_InvalidLogId_ThrowsSqlException()
+        {
+            // Arrange
+            var mockConnectionStrings = new Mock<IConfigurationSection>();
+            mockConnectionStrings.Setup(IConfigurationSection => IConfigurationSection["DefaultConnection"]).Returns("Server=localhost;Database=TestDB;Trusted_Connection=True;");
+            var mockConfiguration = new Mock<IConfiguration>();
+            mockConfiguration.Setup(IConfiguration => IConfiguration.GetSection("ConnectionStrings")).Returns(mockConnectionStrings.Object);
+            var service = new DeleteClinicStatus(mockConfiguration.Object);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<SqlException>(() => service.DeleteAsync(0, "test"));
+        }
+
+        [Fact]
+        public async Task DeleteAsync_InvalidDeletedBy_ThrowsSqlException()
+        {
+            // Arrange
+            var mockConnectionStrings = new Mock<IConfigurationSection>();
+            mockConnectionStrings.Setup(IConfigurationSection => IConfigurationSection["DefaultConnection"]).Returns("Server=localhost;Database=TestDB;Trusted_Connection=True;");
+            var mockConfiguration = new Mock<IConfiguration>();
+            mockConfiguration.Setup(IConfiguration => IConfiguration.GetSection("ConnectionStrings")).Returns(mockConnectionStrings.Object);
+            var service = new DeleteClinicStatus(mockConfiguration.Object);
+
+            // Act & Assert
+            await Assert.ThrowsAsync<SqlException>(() => service.DeleteAsync(1, ""));
         }
     }
 }
